95fe7b6d3ffa3fec69f0cd5d85e2fea84acd239e
afterry <Nikita.Shulepko@mail.ru>
2026-06-25 21:47:14 +0600

    Add task01 solution, string extension and xunit tests

---
+++ practice2026/task01/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            input = input.ToLower();
+
+            StringBuilder builder = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+                {
+                    builder.Append(c);
+                }
+            }
+            string cleaned = builder.ToString();
+
+            char[] charArray = cleaned.ToCharArray();
+            Array.Reverse(charArray);
+            string reversed = new string(charArray);
+
+            return cleaned == reversed;
+        }
+    }
+}
