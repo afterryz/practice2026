1f8d65403c17c971a490c49869a63a0fe5d7d1cb
afterry <Nikita.Shulepko@mail.ru>
2026-06-26 16:49:33 +0600

    Add task04 solution, spaceship implementation and xunit tests

---
+++ practice2026/task04/Fighter.cs
@@ -0,0 +1,23 @@
+﻿namespace task04
+{
+    public class Fighter : ISpaceship
+    {
+        public int Speed => 100;
+        public int FirePower => 50;
+        public int Coordinate { get; set; } = 0;
+        public int Angle { get; set; } = 0;
+        public int Shoot { get; set; } = 0;
+        public void MoveForward()
+        {
+            Coordinate += Speed;
+        }
+        public void Rotate(int angle)
+        {
+            Angle = (Angle + angle) % 360;
+        }
+        public void Fire()
+        {
+            Shoot++;
+        }
+    }
+}
