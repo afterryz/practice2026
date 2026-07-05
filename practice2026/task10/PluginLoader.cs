e1f99ef3104d37072452f7fd0d8e93ab21e3964b
afterry <Nikita.Shulepko@mail.ru>
2026-07-05 19:35:01 +0600

    Add task10 solution, plugin system with topological sort and tests

---
+++ practice2026/task10/PluginLoader.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace task10;
+
+public class PluginLoader
+{
+    public List<Type> DiscoveredPlugins { get; } = new();
+
+    public void LoadPluginsFromDirectory(string directoryPath)
+    {
+        if (!Directory.Exists(directoryPath))
+        {
+            return;
+        }
+
+        string[] dllFiles = Directory.GetFiles(directoryPath, "*.dll");
+
+        foreach (string file in dllFiles)
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(file);
+                IEnumerable<Type> validTypes = assembly.GetTypes()
+                    .Where(t => t.IsClass
+                             && !t.IsAbstract
+                             && typeof(IPlugin).IsAssignableFrom(t)
+                             && t.GetCustomAttribute<PluginLoadAttribute>() != null);
+
+                DiscoveredPlugins.AddRange(validTypes);
+            }
+            catch
+            {
+                continue;
+            }
+        }
+    }
+
+    public void RunAll()
+    {
+        List<Type> sortedTypes = GetTopologicalSort();
+
+        foreach (Type type in sortedTypes)
+        {
+            IPlugin pluginInstance = (IPlugin)Activator.CreateInstance(type)!;
+            pluginInstance.Execute();
+        }
+    }
+
+    private List<Type> GetTopologicalSort()
+    {
+        var sortedList = new List<Type>();
+        var visited = new HashSet<Type>();
+        var processing = new HashSet<Type>();
+
+        void Dfs(Type currentType)
+        {
+            if (visited.Contains(currentType))
+            {
+                return;
+            }
+
+            if (processing.Contains(currentType))
+            {
+                throw new InvalidOperationException("Circular dependency detected.");
+            }
+
+            processing.Add(currentType);
+
+            var attribute = currentType.GetCustomAttribute<PluginLoadAttribute>();
+            string[] dependencies = attribute?.Dependencies ?? Array.Empty<string>();
+
+            foreach (string depName in dependencies)
+            {
+                Type dependentType = DiscoveredPlugins.FirstOrDefault(t => t.Name == depName);
+                if (dependentType != null)
+                {
+                    Dfs(dependentType);
+                }
+            }
+
+            processing.Remove(currentType);
+            visited.Add(currentType);
+            sortedList.Add(currentType);
+        }
+
+        foreach (Type plugin in DiscoveredPlugins)
+        {
+            if (!visited.Contains(plugin))
+            {
+                Dfs(plugin);
+            }
+        }
+
+        return sortedList;
+    }
+}
---
+++ practice2026/task10tests/PluginSystemTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+using task10;
+
+namespace task10tests;
+
+public static class TestExecutionTracker
+{
+    public static List<string> ExecutionOrder = new();
+}
+
+[PluginLoad]
+public class AlphaPlugin : IPlugin
+{
+    public void Execute()
+    {
+        TestExecutionTracker.ExecutionOrder.Add("Alpha");
+    }
+}
+
+[PluginLoad(Dependencies = new[] { "AlphaPlugin" })]
+public class BetaPlugin : IPlugin
+{
+    public void Execute()
+    {
+        TestExecutionTracker.ExecutionOrder.Add("Beta");
+    }
+}
+
+[PluginLoad(Dependencies = new[] { "AlphaPlugin", "BetaPlugin" })]
+public class GammaPlugin : IPlugin
+{
+    public void Execute()
+    {
+        TestExecutionTracker.ExecutionOrder.Add("Gamma");
+    }
+}
+
+public class PluginSystemTests : IDisposable
+{
+    private readonly string _tempDirectory;
+
+    public PluginSystemTests()
+    {
+        _tempDirectory = Path.Combine(Path.GetTempPath(), $"Task10Tests_{Guid.NewGuid()}");
+        Directory.CreateDirectory(_tempDirectory);
+    }
+
+    public void Dispose()
+    {
+        if (Directory.Exists(_tempDirectory))
+        {
+            Directory.Delete(_tempDirectory, true);
+        }
+    }
+
+    [Fact]
+    public void Loader_ExecutesPlugins_InCorrectTopologicalOrder()
+    {
+        TestExecutionTracker.ExecutionOrder.Clear();
+
+        var loader = new PluginLoader();
+        string testAssemblyPath = Path.GetDirectoryName(typeof(PluginSystemTests).Assembly.Location);
+
+        loader.LoadPluginsFromDirectory(testAssemblyPath);
+        loader.RunAll();
+
+        Assert.Equal(new[] { "Alpha", "Beta", "Gamma" }, TestExecutionTracker.ExecutionOrder);
+    }
+
+    [Fact]
+    public void Loader_HandlesEmptyDirectory_WithoutExceptions()
+    {
+        var loader = new PluginLoader();
+        loader.LoadPluginsFromDirectory(_tempDirectory);
+        loader.RunAll();
+
+        Assert.Empty(loader.DiscoveredPlugins);
+    }
+}
