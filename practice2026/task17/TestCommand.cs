732a14131d87003173ffaa8bc2c10d51d5b9a0ff
afterry <Nikita.Shulepko@mail.ru>
2026-07-17 16:56:26 +0600

    Add task19 solution, long operations handling, execution metrics and xunit tests

---
+++ practice2026/task17/TestCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task17;
+
+public class TestCommand : ILongCommand
+{
+    private readonly int _id;
+    private int _counter = 0;
+    private readonly List<int> _executionOrder;
+
+    public TestCommand(int id, List<int> executionOrder)
+    {
+        _id = id;
+        _executionOrder = executionOrder;
+    }
+
+    public bool IsCompleted => _counter >= 3;
+
+    public void Execute()
+    {
+        if (IsCompleted) return;
+
+        _counter++;
+        Console.WriteLine($"Поток {_id} вызов {_counter}");
+
+        lock (_executionOrder)
+        {
+            _executionOrder.Add(_id);
+        }
+    }
+}
